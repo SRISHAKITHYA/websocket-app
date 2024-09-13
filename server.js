292c6c92ee38d76ef1879beaae025d9fde743b1f
srishakithya <srishaki@lentra.ai>
2024-09-13 20:32:21 +0530

    port

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ function heartbeat(ws) {
 }
 
 // Start the server
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
